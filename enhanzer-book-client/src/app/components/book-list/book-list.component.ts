0711ffbde2006650a6b0e7f40de13c10ad3a5005
mohrashard <mohrashard@gmail.com>
2026-03-12 00:23:52 +0530

    feat: Create `BookListComponent` to display, load, delete, and provide a placeholder for editing books.

---
+++ enhanzer-book-client/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book.model';
+
+@Component({
+  selector: 'app-book-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.css'
+})
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
+    this.bookService.getBooks().subscribe({
+      next: (data) => {
+        this.books = data;
+      },
+      error: (err) => {
+        console.error('Error fetching books:', err);
+      }
+    });
+  }
+
+  deleteBook(id: number): void {
+    if (confirm('Are you sure you want to delete this book?')) {
+      this.bookService.deleteBook(id).subscribe({
+        next: () => {
+          this.loadBooks(); // refresh list
+        },
+        error: (err) => {
+          console.error('Error deleting book:', err);
+        }
+      });
+    }
+  }
+
+  editBook(id: number): void {
+    // Placeholder for edit functionality
+    console.log('Edit book clicked for id:', id);
+    alert(`Edit functionality for Book ID ${id} will be implemented next.`);
+  }
+}
